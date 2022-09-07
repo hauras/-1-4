e72a1df6344685f09438e2351d2c5b912f5c5518
하정빈 <@DESKTOP-OQ46K1A>
2022-09-07 21:51:32 +0900

    초기 커밋.

---
+++ 예제 1-4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class PropertyClass
+{
+    private int privateValue;
+    public int PrivateValue
+    {
+        get { return privateValue; }
+        set { privateValue = value; }
+    }
+    public void PrintValue()
+    {
+        Console.WriteLine("hidden Value = " + privateValue);
+    }
+}
+
+class PropertApp
+{
+    public static void Main()
+    {
+        int n;
+        PropertyClass obj = new PropertyClass();
+        obj.PrivateValue = 100;
+        obj.PrintValue();
+        n = obj.PrivateValue;
+        Console.WriteLine("    value = " + n);
+    }
+}
